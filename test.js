205da1b515427a1082d38186cb462f44fdf2babe
CN-Tower <cntower@yahoo.com>
2018-05-23 19:15:31 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+var fn = require('./pxfunc');
+
+// var b = fn.matchPattern('127.0.0.1', 'ip');
+// console.log(b);
+
+fn.log([{name: 'test'}], {title: 'Test', theme: 'red'});
+console.log(fn.time())
