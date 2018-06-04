0065f50833dc26844378ba9c041ad1a2776bd21e
CN-Tower <cntower@yahoo.com>
2018-06-04 11:15:45 +0800

    Update math.ts

--- src/modules/math.ts
+++ src/modules/math.ts
@@ -1,25 +1,25 @@
 export class Mathematic {
     private static array: any;
     /**
-     * [fn.randomId] 返回一个指定长度（最小4位）的随机ID
+     * [fn.rdId] 返回一个指定长度（最小4位）的随机ID
      * @param len 
      */
-    public static randomId(len: number): string {
+    public static rdId(len: number): string {
         const charSet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         let eleId = '';
         if (len < 4) {
             len = 4;
         };
-        this.array(len).forEach(x => eleId += charSet[this.randomNum(charSet.length)]);
+        this.array(len).forEach(x => eleId += charSet[this.rdNum(charSet.length)]);
         return eleId;
     }
 
     /**
-     * 返回一个指定范围内的随机数
+     * [fn.rdNum] 返回一个指定范围内的随机数
      * @param sta 
      * @param end 
      */
-    public static randomNum(sta: number, end?: number): number {
+    public static rdNum(sta: number, end?: number): number {
         if (end === undefined || sta === end) {
             return Math.floor(Math.random() * sta);
         } else {
@@ -33,9 +33,9 @@ export class Mathematic {
     }
 
     /**
-     * 返回一个随机颜色色值
+     * [fn.rdColor] 返回一个随机颜色色值
      */
-    public static randomColor() {
-        return '#' + ('00000' + (this.randomNum(0x1000000) << 0).toString(16)).slice(-6);
+    public static rdColor() {
+        return '#' + ('00000' + (this.rdNum(0x1000000) << 0).toString(16)).slice(-6);
     }
 }
