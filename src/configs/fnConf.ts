893111674e839519993d9406d512c0267f5633e7
CN-Tower <cntower@yahoo.com>
2018-06-04 11:12:36 +0800

    Create fnConf.ts

---
+++ src/configs/fnConf.ts
@@ -0,0 +1,15 @@
+export const FN_CONF = {
+    clientMethods: [
+        'setCookie',
+        'getCookie',
+        'removeCookie',
+        'fullScreen',
+        'exitFullScreen',
+        'checkIsFullScreen',
+    ],
+    serverMethods: [
+        'initProgress',
+        'initTools'
+    ],
+    isClient: true
+}
