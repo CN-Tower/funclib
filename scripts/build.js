d7cbde398499e26591451f477e44447b9796e9b6
CN-Tower <cntower@yahoo.com>
2018-06-05 21:59:57 +0800

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -2,9 +2,23 @@ const fn = require('funclib');
 const path = require('path');
 const webpack = require('webpack');
 const config = require('./webpack.conf');
-const funclibMinJs = path.resolve(__dirname, '../', 'dist', 'funclib.min.js');
 
-fn.rm(funclibMinJs);
+const root = path.dirname(__dirname);
+const fnMinJs = path.join(root, 'dist/funclib.min.js');
+const DtsDist = path.join(root, 'dist/funclib.d.ts');
+const indexDist = path.join(root, 'dist/index.js');
+const pkgDist = path.join(root, 'dist/package.json');
+const rdmeDist = path.join(root, 'dist/README.md');
+const DtsSrc = path.join(root, 'src/assets/funclib.d.ts');
+const indexSrc = path.join(root, 'src/assets/index.js');
+const pkgSrc = path.join(root, 'src/assets/package.json');
+const rdmeSrc = path.join(root, 'README.md');
+
+const srcs = [DtsSrc, indexSrc, pkgSrc, rdmeSrc];
+const dists = [DtsDist, indexDist, pkgDist, rdmeDist, fnMinJs];
+dists.forEach(f => fn.rm(f));
+srcs.forEach((f, i) => fn.cp(f, dists[i]));
+
 fn.progress.start({title: 'Compiling Funclib', width: 41});
 webpack(config, function (err, stats) {
   if (err) throw (err);
@@ -23,14 +37,14 @@ webpack(config, function (err, stats) {
 });
 
 function buildFix() {
-  const funclibMin = fn.rd(funclibMinJs);
+  const funclibMin = fn.rd(fnMinJs);
   const idx = '!function('.length;
   const rt = funclibMin.substr(idx, 1);
   const ft = funclibMin.substr(idx + 2, 1);
   const it = `new (${ft}().Funclib)(${rt})`;
-  const newFunclibMin = funclibMin
+  const newFnMin = funclibMin
     .replace(new RegExp(`module\.exports=${ft}\\(\\)`), `module.exports=${it}`)
     .replace(new RegExp(`define\\(\\[\\],${ft}\\)`), `define([${rt}], function(${rt}) {${it}})`)
     .replace(new RegExp(`\.fn=${ft}\\(\\)`, 'mg'), `.fn=${it}`);
-  fn.wt(funclibMinJs, newFunclibMin);
+  fn.wt(fnMinJs, newFnMin);
 }
