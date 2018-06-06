3bb7878175e318b6a879c2ac54138bf2dcd2c234
CN-Tower <cntower@yahoo.com>
2018-06-06 14:47:13 +0800

    Update loger.ts

--- src/modules/loger.ts
+++ src/modules/loger.ts
@@ -1,18 +1,29 @@
-let chalk;
-
 export class Loger {
     private static array;
     private static version;
     private static cutString;
+    
+    private static colors = {
+        'grey': '\x1B[90m%s\x1B[0m',
+        'blue': '\x1B[34m%s\x1B[0m',
+        'cyan': '\x1B[36m%s\x1B[0m',
+        'green': '\x1B[32m%s\x1B[0m',
+        'magenta': '\x1B[35m%s\x1B[0m',
+        'red': '\x1B[31m%s\x1B[0m',
+        'yellow': '\x1B[33m%s\x1B[0m',
+        'default': '%s',
+    }
 
     /**
      * [fn.chalk] 在控制台打印有颜色的字符串
      * @param value 
      * @param color 
      */
-    public static chalk(value: string, color) {
-        chalk = eval('require("chalk")');
-        color ? console.log(chalk[color](value)) : console.log(value);
+    public static chalk(value: string, color?: string) {
+        if (!(color in this.colors)) {
+            color = 'default'
+        }
+        return this.colors[color].replace(/%s/, value);
     }
 
   /**
@@ -21,47 +32,45 @@ export class Loger {
    * @param configs {
    * title: string,
    * lineLen: number [20-100]
-   * part: 'pre'|'end' [S]
-   * color: 'grey'|'blue'|'cyan'|'green'|'magenta'|'red'|'yellow' [S] }
+   * part: 'pre'|'end'
+   * color: 'grey'|'blue'|'cyan'|'green'|'magenta'|'red'|'yellow'
+   * ttColor: 'grey'|'blue'|'cyan'|'green'|'magenta'|'red'|'yellow' }
    */
     public static log (value: any, configs: Object, isClient: boolean) {
-        if (value === undefined) {
-            value = `Welecome come to use funclib: ${this.version} !`;
-        }
-        if (typeof value === 'object') {
-            value = JSON.stringify(value, null, 2);
+        value = typeof value === 'object'
+            ? JSON.stringify(value, null, 2)
+            : String(value);
+        if (configs && typeof configs === 'string') {
+            isClient ? console.log(value) : console.log(this.chalk(value, configs));
         } else {
-            value = String(value);
+            let title = configs && configs['title'] || `funclib ${this.version}`;
+            let lineLen = configs && configs['lineLen'];
+            if (!lineLen || lineLen < 20 || lineLen > 100) {
+                lineLen = 66;
+            }
+            let titlelen, sp = '';
+            if (title.length <= lineLen - 10) {
+                titlelen = title.length;
+            } else {
+                titlelen = lineLen - 10;
+                title = this.cutString(title, titlelen - 2);
+            }
+            this.array((lineLen - titlelen) / 2, ' ').forEach(x => sp += x);
+            const tt = sp + title;
+            const s = '-', d = '=';
+            let sL = '', dL = '';
+            this.array(lineLen).forEach(x => {
+                sL += s;
+                dL += d;
+            });
+            isClient
+                ? Loger.clientLog(dL, tt, sL, value)
+                : Loger.serverLog(dL, tt, sL, value, configs);    
         }
-        let title = configs && configs['title'] || `funclib ${this.version}`;
-        let lineLen = configs && configs['lineLen'];
-        if (!lineLen || lineLen < 20 || lineLen > 100) {
-            lineLen = 66;
-        }
-        let titlelen, sp = '';
-        if (title.length <= lineLen - 10) {
-            titlelen = title.length;
-        } else {
-            titlelen = lineLen - 10;
-            title = this.cutString(title, titlelen - 2);
-        }
-        this.array((lineLen - titlelen) / 2, ' ').forEach(x => sp += x);
-        const tt = sp + title;
-        const s = '-', d = '=';
-        let sL = '', dL = '';
-        this.array(lineLen).forEach(x => {
-            sL += s;
-            dL += d;
-        });
-        isClient ? Loger.clientLog(dL, tt, sL, value) : Loger.serverLog(dL, tt, sL, value, configs);
     }
 
     /**
      * 客户端打印
-     * @param dL 
-     * @param tt 
-     * @param sL 
-     * @param value 
      */
     private static clientLog(dL, tt, sL, value)  {
         console.log(`\n${dL}\n${tt}\n${sL}\n${value}\n${dL}\n`);
@@ -69,42 +78,28 @@ export class Loger {
 
     /**
      * 服务端打印
-     * @param dL 
-     * @param tt 
-     * @param sL 
-     * @param value 
-     * @param configs 
      */
     private static serverLog(dL, tt, sL, value, configs)  {
-        chalk = eval('require("chalk")');
-        const colors = {
-            'grey': '\x1B[90m%s\x1B[0m',
-            'blue': '\x1B[34m%s\x1B[0m',
-            'cyan': '\x1B[36m%s\x1B[0m',
-            'green': '\x1B[32m%s\x1B[0m',
-            'magenta': '\x1B[35m%s\x1B[0m',
-            'red': '\x1B[31m%s\x1B[0m',
-            'yellow': '\x1B[33m%s\x1B[0m'
-        }
-        const color = configs && configs['color'] in colors && configs['color'] || 'grey';
+        const color = configs
+                      && configs['color'] in this.colors
+                      && configs['color'] || 'grey';
+        const ttColor = configs
+                        && configs['ttColor'] in this.colors
+                        && configs['ttColor'] || 'green';
+
         if (configs && ['pre', 'end'].indexOf(configs['part']) > -1) {
             if (configs['part'] === 'pre') {
                 console.log('\n' + dL);
-                // console.log(colors['green'], tt);
-                console.log(chalk.green(tt));
+                console.log(this.chalk(tt, ttColor));
                 console.log(sL);
             } else {
                 console.log(dL + '\n');
             }
-        } else if (configs && configs['isFmt'] === false) {
-            console.log(colors['green'], tt);
         } else {
             console.log('\n' + dL);
-            // console.log(colors['green'], tt);
-            console.log(chalk.green(tt));
+            console.log(this.chalk(tt, ttColor));
             console.log(sL);
-            // console.log(colors[color], value);
-            console.log(chalk[color](value));
+            console.log(this.chalk(value, color));
             console.log(dL + '\n');
         }
     }
