94ab01f2092761b7c21e8f3de734f5ce1192e061
CN-Tower <cntower@yahoo.com>
2018-07-20 16:57:46 +0800

    Update tricks.ts

--- src/modules/tricks.ts
+++ src/modules/tricks.ts
@@ -1,23 +1,25 @@
 export class Tricks {
     public static extendJquery(jquery?: any) {
-        const $ = jquery || window['$'] || window['jquery'];
-        // $.extend({});
-        /**
-         * [$ele.findCousin] 寻找元素的表亲
-         * @param selector [string]
-         * @param level    [number]
-         */
-        $.fn.findCousin = function (selector: string, level: number = 0) {
-            if (!level) {
-            return selector ? this.parents().find(selector) : this.parents();
-            } else {
-            let $parent = this;
-            for (let i = 0; i < level; i++) {
-                $parent = $parent.parent();
-            }
-            return selector ? $parent.find(selector) : $parent;
-            }
-        };
+        const $ = jquery || window['jquery'] || window['jQuery'] || null;
+        if ($) {
+            // $.extend({});
+            /**
+             * [$ele.findCousin] 寻找元素的表亲
+             * @param selector [string]
+             * @param level    [number]
+             */
+            $.fn.findCousin = function (selector: string, level: number = 0) {
+                if (!level) {
+                return selector ? this.parents().find(selector) : this.parents();
+                } else {
+                let $parent = this;
+                for (let i = 0; i < level; i++) {
+                    $parent = $parent.parent();
+                }
+                return selector ? $parent.find(selector) : $parent;
+                }
+            };
+        }
         return !!$;
     }
 }
