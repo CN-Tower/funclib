41da833551f83991bb38a4236639704f63b02ef6
CN-Tower <cntower@yahoo.com>
2018-08-31 15:04:49 +0800

    Update type.spec.js

--- test/type.spec.js
+++ test/type.spec.js
@@ -1,25 +1,25 @@
 describe('Type Methods:', function () {
-    describe('#fn.typeOf()', function () {
-        it(`fn.typeOf('string', 'str') should return true.`, function () {
-            assert(fn.typeOf('string', 'str') === true);
-        });
-        it(`fn.typeOf([], 'obj') should return false.`, function () {
-            assert(fn.typeOf([], 'obj') === false);
-        });        
-        it(`fn.typeOf({}, ['str', 'obj']) should return true.`, function () {
-            assert(fn.typeOf({}, ['str', 'obj']) === true);
-        });
+  describe('#fn.typeOf()', function () {
+    it(`fn.typeOf('string', 'str') should return true.`, function () {
+      assert(fn.typeOf('string', 'str') === true);
     });
-    describe('#fn.typeVal()', function () {
-        it(`fn.typeVal('string', 'str') should return string.`, function () {
-            assert(fn.typeVal('string', 'str') === 'string');
-        });
-        it(`fn.typeVal([], 'obj') should return false.`, function () {
-            assert(fn.typeVal([], 'obj') === false);
-        });        
-        it(`fn.typeVal({}, ['str', 'obj']) should return {}.`, function () {
-            const obj = fn.typeVal({}, ['str', 'obj']);
-            assert(fn.typeOf(obj, 'obj') && fn.len(obj) === 0);
-        });
+    it(`fn.typeOf([], 'obj') should return false.`, function () {
+      assert(fn.typeOf([], 'obj') === false);
     });
+    it(`fn.typeOf({}, ['str', 'obj']) should return true.`, function () {
+      assert(fn.typeOf({}, ['str', 'obj']) === true);
+    });
+  });
+  describe('#fn.typeVal()', function () {
+    it(`fn.typeVal('string', 'str') should return string.`, function () {
+      assert(fn.typeVal('string', 'str') === 'string');
+    });
+    it(`fn.typeVal([], 'obj') should return false.`, function () {
+      assert(fn.typeVal([], 'obj') === false);
+    });
+    it(`fn.typeVal({}, ['str', 'obj']) should return {}.`, function () {
+      const obj = fn.typeVal({}, ['str', 'obj']);
+      assert(fn.typeOf(obj, 'obj') && fn.len(obj) === 0);
+    });
+  });
 });
