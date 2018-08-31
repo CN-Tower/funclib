cd4be4b88f10188a0876f8cb1d2c4fab9a32dcb8
CN-Tower <cntower@yahoo.com>
2018-08-31 15:00:11 +0800

    Update _Trick.ts

--- src/modules/_Trick.ts
+++ src/modules/_Trick.ts
@@ -1,16 +1,16 @@
 export class FnTrick {
-    /**
-     * [fn.copyText] 复制文本到粘贴板
-     * @param text [string]
-     */
-    public static copyText(text: string = ''): void {
-        let textarea = document.createElement('textarea');
-        textarea.style.position = 'fixed';
-        textarea.style.left = '200%';
-        document.body.appendChild(textarea)
-        textarea.value = text;
-        textarea.select();
-        document.execCommand('Copy');
-        document.body.removeChild(textarea);
-    }
+  /**
+   * [fn.copyText] 复制文本到粘贴板
+   * @param text [string]
+   */
+  public static copyText(text: string = ''): void {
+    let textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.left = '200%';
+    document.body.appendChild(textarea)
+    textarea.value = text;
+    textarea.select();
+    document.execCommand('Copy');
+    document.body.removeChild(textarea);
+  }
 }
