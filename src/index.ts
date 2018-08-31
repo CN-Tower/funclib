ca203e7af5bc817aeeea3e2597015f243094d110
CN-Tower <cntower@yahoo.com>
2018-08-31 14:55:35 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,21 @@ import { FnTime } from './modules/_Time';
 import { FnRegExp } from './modules/_RegExp';
 import { FnMath } from './modules/_Math';
 import { FnFunction } from './modules/_Function';
-import { FnUrl } from './modules/_Url';
 import { FnFileSys } from './modules/_FileSys';
 import { FnProgress } from './modules/_Progress';
 import { FnLog } from './modules/_Logs';
 import { VERSION, MAIN_METHODS } from './funclib.conf'
 
 const fnModules = [
-  FnType, FnArray, FnObject, FnString, FnTime, FnRegExp,
-  FnMath, FnFunction, FnUrl, FnFileSys, FnLog
-] 
-const methods = [...MAIN_METHODS, 'chalk', 'rd', 'wt', 'cp', 'mv', 'rm', 'mk' ];
+  FnType, FnArray, FnObject, FnString, FnTime,
+  FnRegExp, FnMath, FnFunction, FnFileSys, FnLog
+]
+const methods = [...MAIN_METHODS, 'chalk', 'rd', 'wt', 'cp', 'mv', 'rm', 'mk'];
 
 let _fn: any = {};
 fnModules.forEach(fnModule => {
   FnObject.forIn(fnModule, (mtd, method) => {
-    if (methods.indexOf(mtd) > -1) _fn[mtd] =  function() {
+    if (methods.indexOf(mtd) > -1) _fn[mtd] = function () {
       let args: any = arguments;
       args = Object.keys(args).map(key => args[key]);
       return _fn.data !== undefined ? method(_fn.data, ...args) : method(...args);
@@ -29,7 +28,7 @@ fnModules.forEach(fnModule => {
   });
 });
 
-var fn: any = function(data: any) {
+var fn: any = function (data: any) {
   _fn.data = data;
   return _fn;
 }
