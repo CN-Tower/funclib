b43434e6affa79c5926ad50fa4b234533975feea
CN-Tower <cntower@yahoo.com>
2018-08-31 15:03:41 +0800

    Update pattern.spec.js

--- test/pattern.spec.js
+++ test/pattern.spec.js
@@ -1,19 +1,19 @@
 describe('RegExp Methods:', function () {
-    describe('#fn.getPattern()', function () {
-        it(`fn.getPattern("email") should return a pattern.`, function () {
-            const pattern = fn.getPattern("email");
-            assert(pattern instanceof RegExp);
-        });
+  describe('#fn.getPattern()', function () {
+    it(`fn.getPattern("email") should return a pattern.`, function () {
+      const pattern = fn.getPattern("email");
+      assert(pattern instanceof RegExp);
     });
-    describe('#fn.matchPattern()', function () {
-        it(`fn.matchPattern(email, 'email') should return true.`, function () {
-            const email = 'cntower@yahoo.com'
-            assert(fn.matchPattern(email, 'email') === true);
-        });
-        it(`fn.matchPattern(mobPhone, 'email') should return false.`, function () {
-            const mobPhone = '18770347138'
-            const email = 'cntower@yahoo.com'
-            assert(fn.matchPattern(mobPhone, 'email') === false);
-        });
+  });
+  describe('#fn.matchPattern()', function () {
+    it(`fn.matchPattern(email, 'email') should return true.`, function () {
+      const email = 'cntower@yahoo.com'
+      assert(fn.matchPattern(email, 'email'));
     });
+    it(`fn.matchPattern(mobPhone, 'email') should return false.`, function () {
+      const mobPhone = '18770347138'
+      const email = 'cntower@yahoo.com'
+      assert(!fn.matchPattern(mobPhone, 'email'));
+    });
+  });
 });
