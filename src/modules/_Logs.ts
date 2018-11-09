be24ce4d7537e68c56fd722894b0c08e2ec78a8b
CN-Tower <cntower@yahoo.com>
2018-11-09 14:30:33 +0800

    Update _Logs.ts

--- src/modules/_Logs.ts
+++ src/modules/_Logs.ts
@@ -6,18 +6,20 @@ import { FnType } from './_Type';
 import { VERSION } from '../funclib.conf';
 
 const COLOR_LIST = {
-  'grey': '\x1B[90m%s\x1B[0m',
-  'blue': '\x1B[34m%s\x1B[0m',
-  'cyan': '\x1B[36m%s\x1B[0m',
-  'green': '\x1B[32m%s\x1B[0m',
+  'grey'   : '\x1B[90m%s\x1B[0m',
+  'blue'   : '\x1B[34m%s\x1B[0m',
+  'cyan'   : '\x1B[36m%s\x1B[0m',
+  'green'  : '\x1B[32m%s\x1B[0m',
   'magenta': '\x1B[35m%s\x1B[0m',
-  'red': '\x1B[31m%s\x1B[0m',
-  'yellow': '\x1B[33m%s\x1B[0m',
+  'red'    : '\x1B[31m%s\x1B[0m',
+  'yellow' : '\x1B[33m%s\x1B[0m',
   'default': '%s\x1B[0m'
 }
 
+const getIsFmt = configs => FnObject.has(configs, 'isFmt') ? configs.isFmt : true;
+const getTitle = configs => FnObject.get(configs, '/title') || `funclib(${VERSION})`;
+
 export class FnLog {
-  
   /**
    * [fn.chalk] 在控制台打印有颜色的字符串
    * @param value 
@@ -31,25 +33,38 @@ export class FnLog {
   /**
    * [fn.log] 控制台格式化打印值
    * @param value 
+   * @param title
    * @param configs 
    * {title: string, width: number [20-100], isFmt: boolean
    * pre: boolean = false, end: boolean = false
    * color: 'grey'|'blue'|'cyan'|'green'|'magenta'|'red'|'yellow'
    * ttColor: 'grey'|'blue'|'cyan'|'green'|'magenta'|'red'|'yellow'}
-   * @param isFmt 
    */
-  public static log(value: any, configs?: any, isFmt: boolean = true) {
-    if (configs && typeof configs.isFmt === 'boolean') isFmt = configs.isFmt;
-    if (typeof configs === 'boolean') {
-      isFmt = configs;
-      configs = undefined;
+  public static log(value: any, title?: any, configs?: any) {
+    let isFmt;
+    if (FnType.typeVal(title, 'str')) {
+      if (FnType.typeOf(configs, 'bol')) {
+        isFmt = configs;
+        configs = undefined;
+      } else {
+        isFmt = getIsFmt(configs);
+      }
+    } else if (FnType.typeOf(title, 'bol')) {
+      isFmt = title;
+      title = getTitle(configs);
+    } else if (FnType.typeOf(title, 'obj')) {
+      configs = title;
+      isFmt = getIsFmt(configs);
+      title = getTitle(configs);
+    } else {
+      isFmt = true;
+      title = `funclib(${VERSION})`;
     }
     // Value
     value = FnString.pretty(value);
     // Title
     let time = `[${FnTime.fmtDate('hh:mm:ss')}] `;
-    let title = (FnType.typeVal(configs, 'str') || FnObject.get(configs, '/title')
-      || `funclib(${VERSION})`).replace(/\n/mg, '');
+    title = title.replace(/\n/mg, '');
     const originTtLength = (time + title + '[] ').length;
     if (!isFmt) title = `( ${title} )`;
     time = FnLog.chalk(time);
