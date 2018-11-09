0fc445fcc1db8f30fa31de376a0b3c651c081349
CN-Tower <cntower@yahoo.com>
2018-11-09 14:33:00 +0800

    Update dom.spec.js

--- test/client-methods/dom.spec.js
+++ test/client-methods/dom.spec.js
@@ -1,27 +1,23 @@
 describe('Dom Methods:', function () {
-    describe('#fn.fullScreen()', function () {
-        before(function() {
-            fn.fullScreen($('html')[0]);
-        });
-        it(`fn.fullScreen() should make the element full screen.`, function () {
-            setTimeout(function() {
-                assert(fn.isFullScreen($('html')[0]) === true);
-            });
-        });
+  describe('#fn.fullScreen()', function () {
+    before(function () {
+      fn.fullScreen($('html')[0]);
     });
-    describe('#fn.exitFullScreen()', function () {
-        before(function() {
-            fn.exitFullScreen($('html')[0]);
-        });
-        it(`fn.exitFullScreen() should make the element exit full screen.`, function () {
-            setTimeout(function() {
-                assert(fn.isFullScreen($('html')[0]) === false);
-            });
-        });
+    it(`fn.fullScreen() should make the element full screen.`, function () {
+      fn.defer(() => assert(fn.isFullScreen() === true));
     });
-    describe('#fn.isFullScreen()', function () {
-        it(`fn.isFullScreen() check is the element full screen.`, function () {
-            assert(fn.isFullScreen($('html')[0]) === false);
-        });
+  });
+  describe('#fn.exitFullScreen()', function () {
+    before(function () {
+      fn.exitFullScreen($('html')[0]);
     });
+    it(`fn.exitFullScreen() should make the element exit full screen.`, function () {
+      fn.defer(() => assert(fn.isFullScreen() === false));
+    });
+  });
+  describe('#fn.isFullScreen()', function () {
+    it(`fn.isFullScreen() check is the element full screen.`, function () {
+      assert(fn.isFullScreen() === false);
+    });
+  });
 });
