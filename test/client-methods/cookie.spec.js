94055ce7c8924958483599ed87f5c4e79d9caed7
CN-Tower <cntower@yahoo.com>
2018-11-09 14:32:48 +0800

    Update cookie.spec.js

--- test/client-methods/cookie.spec.js
+++ test/client-methods/cookie.spec.js
@@ -1,29 +1,29 @@
 describe('Cookie Methods:', function () {
-    describe('#fn.setCookie()', function () {
-        before(function() {
-            fn.setCookie('user', 'Tom');
-        });
-        it.skip(`fn.setCookie() should set a cookie.`, function () {
-            assert(fn.getCookie('user') === 'Tom');
-        });
+  describe('#fn.setCookie()', function () {
+    before(function () {
+      fn.setCookie('user', 'Tom');
     });
-    describe('#fn.getCookie()', function () {
-        before(function() {
-            fn.setCookie('pwd', 'Tom_123');
-        });
-        it.skip(`fn.getCookie() should got a cookie.`, function () {
-            assert(fn.getCookie('pwd') === 'Tom_123');
-        });
+    it.skip(`fn.setCookie() should set a cookie.`, function () {
+      assert(fn.getCookie('user') === 'Tom');
     });
-    describe('#fn.removeCookie()', function () {
-        before(function() {
-            fn.removeCookie('user');
-            fn.removeCookie('pwd');
-        });
-        it.skip(`fn.removeCookie() should remove a cookie.`, function () {
-            const user = fn.getCookie('user');
-            const pwd = fn.getCookie('pwd');
-            assert(!!user === false && !!pwd === false);
-        });
+  });
+  describe('#fn.getCookie()', function () {
+    before(function () {
+      fn.setCookie('pwd', 'Tom_123');
     });
+    it.skip(`fn.getCookie() should got a cookie.`, function () {
+      assert(fn.getCookie('pwd') === 'Tom_123');
+    });
+  });
+  describe('#fn.removeCookie()', function () {
+    before(function () {
+      fn.removeCookie('user');
+      fn.removeCookie('pwd');
+    });
+    it.skip(`fn.removeCookie() should remove a cookie.`, function () {
+      const user = fn.getCookie('user');
+      const pwd = fn.getCookie('pwd');
+      assert(!!user === false && !!pwd === false);
+    });
+  });
 });
