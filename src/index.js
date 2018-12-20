56d207e6d75046d6b85dc40529d1e2efb95d3caa
CN-Tower <247114045@qq.com>
2018-12-20 13:27:42 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1458,7 +1458,7 @@
     }
 
     /**
-     * [fn.size] 获取文件的大小(kb)
+     * [fn.size] 获取文件的大小
      * @param file   : string
      * @param unit  : 'b'|'kb'|'mb'|'gb'|'tb' = 'kb'
      * @param digit : number = 2
