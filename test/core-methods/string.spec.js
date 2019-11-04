39310b284b567ca244089b96052317eac9f22d94
JacqueLine <jacqueline@flashcash4u.com>
2019-11-04 18:56:34 +0800

    fix maskString

--- test/core-methods/string.spec.js
+++ test/core-methods/string.spec.js
@@ -78,8 +78,8 @@
       it(`fn.maskString(18770347037, 3) should return '187********'.`, function () {
         assert(fn.maskString(18770347037, 3) === '187********');
       });
-      it(`fn.maskString('18770347037', 3, 4) should return '187****7037'.`, function () {
-        assert(fn.maskString('18770347037', 'x', 3, 4) === '187xxxx7037');
+      it(`fn.maskString('18770347037', 3, 4, 'x') should return '187****7037'.`, function () {
+        assert(fn.maskString('18770347037', 3, 4, 'x') === '187xxxx7037');
       });
       it(`fn.maskString('测试双字节', 2, 1) should return '测试**字节'.`, function () {
         assert(fn.maskString('测试双字节', 2, 1) === '测试**字节');
